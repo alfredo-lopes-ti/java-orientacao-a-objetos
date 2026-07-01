9685dd0571de7f9ab8b47ee81e35253d40ef824a
Alfredo Lopes <AlfredoLopes7@protonmail.com>
2026-07-01 19:17:18 -0300

    Conceito: Encapsulamento completo com Getter e Setter no projeto Square

--- src/main/java/01-introducao-oo/area-quadrado/src/entities/Square.java
+++ src/main/java/01-introducao-oo/area-quadrado/src/entities/Square.java
@@ -2,7 +2,18 @@ package entities;
 
 public class Square {
     // Atributos (as características do quadrado
-    public double side;
+    private double side;
+
+    public void setSide(double side) {
+        if(side <= 0) {
+            throw new IllegalArgumentException("Invalid side value! Must be greater than zero.");
+        }
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
 
     // Metodo (a ação que o quadrado sabe fazer: aqui, calcular sua área)
     public double area() {
